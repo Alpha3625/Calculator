e1ede3ac22fa22202f54a3ae97ceb50fbaa52f02
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2022-08-18 13:35:52 +0400

    commit(1)

---
+++ App.js
@@ -0,0 +1,27 @@
+/*function operations (num1, num2, operation) {
+    switch(operation){
+        case '/':
+            alert(`${num1} / ${num2} = ${num1 / num2}`);
+            break;
+        case '*':
+            alert(`${num1} * ${num2} = ${num1 * num2}`);
+            break;
+        case '-':
+            alert(`${num1} - ${num2} = ${num1 - num2}`);
+            break;
+        case '+':
+            alert(`${num1} + ${num2} = ${num1 + num2}`);
+            break;
+        case '%':
+            alert(`${num1} % ${num2} = ${num1 % num2}`);
+            break;
+        default:
+            alert('Error!');
+    }
+}
+
+num1 = +prompt();
+operation = prompt();
+num2 = +prompt();
+operations (num1, num2, operation)
+*/
